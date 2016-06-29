5fef69146a22ba578727a9a3f4508c18ea862303
Oclemy <oclemmi@gmail.com>
2016-06-29 16:51:38 -0700

    Initial commit

---
+++ app/src/main/java/com/tutorials/hp/firebaselistviewmulti_items/m_Model/Spacecraft.java
@@ -0,0 +1,35 @@
+package com.tutorials.hp.firebaselistviewmulti_items.m_Model;
+
+/**
+ * Created by Oclemy on 6/21/2016 for ProgrammingWizards Channel and http://www.camposha.com.
+ */
+public class Spacecraft {
+    String name,propellant,description;
+
+    public Spacecraft() {
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getPropellant() {
+        return propellant;
+    }
+
+    public void setPropellant(String propellant) {
+        this.propellant = propellant;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+}
---
+++ app/src/main/java/com/tutorials/hp/firebaselistviewmulti_items/m_UI/CustomAdapter.java
@@ -0,0 +1,71 @@
+package com.tutorials.hp.firebaselistviewmulti_items.m_UI;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.tutorials.hp.firebaselistviewmulti_items.R;
+import com.tutorials.hp.firebaselistviewmulti_items.m_Model.Spacecraft;
+
+import java.util.ArrayList;
+
+/**
+ * Created by Oclemy on 6/21/2016 for ProgrammingWizards Channel and http://www.camposha.com.
+ * 1. where WE INFLATE OUR MODEL LAYOUT INTO VIEW ITEM
+ * 2. THEN BIND DATA
+ */
+public class CustomAdapter extends BaseAdapter{
+    Context c;
+    ArrayList<Spacecraft> spacecrafts;
+
+    public CustomAdapter(Context c, ArrayList<Spacecraft> spacecrafts) {
+        this.c = c;
+        this.spacecrafts = spacecrafts;
+    }
+
+    @Override
+    public int getCount() {
+        return spacecrafts.size();
+    }
+
+    @Override
+    public Object getItem(int position) {
+        return spacecrafts.get(position);
+    }
+
+    @Override
+    public long getItemId(int position) {
+        return position;
+    }
+
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+        if(convertView==null)
+        {
+            convertView= LayoutInflater.from(c).inflate(R.layout.model,parent,false);
+        }
+
+        TextView nameTxt= (TextView) convertView.findViewById(R.id.nameTxt);
+        TextView propTxt= (TextView) convertView.findViewById(R.id.propellantTxt);
+        TextView descTxt= (TextView) convertView.findViewById(R.id.descTxt);
+
+        final Spacecraft s= (Spacecraft) this.getItem(position);
+
+        nameTxt.setText(s.getName());
+        propTxt.setText(s.getPropellant());
+        descTxt.setText(s.getDescription());
+
+        //ONITECLICK
+        convertView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(c,s.getName(),Toast.LENGTH_SHORT).show();
+            }
+        });
+        return convertView;
+    }
+}
